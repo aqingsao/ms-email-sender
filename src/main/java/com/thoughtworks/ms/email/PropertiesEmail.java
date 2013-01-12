d15fab00534c09e4e87b79b95034def3c213fe09
aqingsao <xqzhang@thoughtworks.com>
2013-01-13 01:55:05 +0800

    [xiaoqing] Remove unused properties in PropertiesEmail.

--- src/main/java/com/thoughtworks/ms/email/PropertiesEmail.java
+++ src/main/java/com/thoughtworks/ms/email/PropertiesEmail.java
@@ -18,31 +18,6 @@ public class PropertiesEmail {
 
     public String authenticationPassword = null;
 
-    //Others properties
-    public final String MAIL_SERVER = "mail.server";
-
-    public final String MAIL_FROM = "mail.from";
-
-    public final String MAIL_PORT = "mail.port";
-
-    public final String MAIL_USER = "mail.user";
-
-    public final String MAIL_PASSWORD = "mail.password";
-
-    public final String MAIL_AUTENTICATION = "mail.autentication";
-
-    public final String MAIL_ISSUE = "mail.issue";
-
-    public final String MAIL_CC = "mail.cc";
-
-    public final String MAIL_BCC = "mail.bcc";
-
-    public final String MAIL_ACTIVATE_BCC = "mail.activatebcc";
-
-    public final String MAIL_ADDRESS_SENDER = "mail.address.sender";
-
-    public final String MAIL_NAME_SENDER = "mail.name.sender";
-
     public PropertiesEmail() {
 
     }
@@ -77,42 +52,6 @@ public class PropertiesEmail {
 
     public void setAuthenticationNeeded(Boolean mAIL_AUTENTICATION_VALUE) {
         authenticationNeeded = mAIL_AUTENTICATION_VALUE;
-    }
-
-    public String getMAIL_SERVER() {
-        return MAIL_SERVER;
-    }
-
-    public String getMAIL_FROM() {
-        return MAIL_FROM;
-    }
-
-    public String getMAIL_PORT() {
-        return MAIL_PORT;
-    }
-
-    public String getMAIL_USER() {
-        return MAIL_USER;
-    }
-
-    public String getMAIL_PASSWORD() {
-        return MAIL_PASSWORD;
-    }
-
-    public String getMAIL_AUTENTICATION() {
-        return MAIL_AUTENTICATION;
-    }
-
-    public String getMAIL_ISSUE() {
-        return MAIL_ISSUE;
-    }
-
-    public String getMAIL_ADDRESS_SENDER() {
-        return MAIL_ADDRESS_SENDER;
-    }
-
-    public String getMAIL_NAME_SENDER() {
-        return MAIL_NAME_SENDER;
     }
 
     boolean isAuthenticationRequired() {
